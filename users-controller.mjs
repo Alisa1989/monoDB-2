b64be49e928a20e432796bfcf2081e99d1a66dd8
Alexandre Steinhauslin <a.steinhauslin@gmail.com>
2023-06-03 21:14:46 -0700

    added create and retrieve and tested them

--- users-controller.mjs
+++ users-controller.mjs
@@ -10,3 +10,41 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
+
+// CREATE controller--------------------
+app.get('/create', asyncHandler(async (req,res) => {
+    const user = await users.createUser(
+        req.query.name,
+        req.query.age,
+        req.query.email,
+        req.query.phoneNumber
+    )
+    res.send(user)
+}));
+
+// RETRIEVE controller--------------------
+function userFilter(req) {
+    let filter = {};
+    if (req.query._id !== undefined) {
+        filter._id = req.query._id;
+    } if (req.query.name !== undefined) {
+         filter.name = req.query.name;
+    } if (req.query.age !== undefined) {
+         filter.age = req.query.age;
+    } if (req.query.email !== undefined) {
+        filter.email = req.query.email; 
+    } if (req.query.phoneNumber !== undefined) {
+        filter.phoneNumber = req.query.phoneNumber; 
+    }
+    return filter;
+}
+
+app.get('/retrieve', asyncHandler(async (req, res) => {
+    const filter = userFilter(req);
+    const result = await users.findUsers(filter)
+    res.send(result);
+}))
+
+
+// UPDATE controller--------------------
+// DELETE controller--------------------
--- users-model.mjs
+++ users-model.mjs
@@ -11,3 +11,41 @@ const db = mongoose.connection;
 db.once("open", () => {
     console.log("Successfully connected to MongoDB using Mongoose!");
 });
+
+//  Collection Schema
+const userSchema = mongoose.Schema({
+    name: { type: String, required: true},
+    age: { type: Number, required: true},
+    email: { type: String, required: true},
+    phoneNumber: { type: String, required: false}
+});
+
+const User = mongoose.model("User", userSchema)
+
+// CREATE models---------------------------
+const createUser = async (name, age, email, phoneNumber) => {
+    const user = new User({
+        name: name,
+        age: age,
+        email: email,
+        phoneNumber: phoneNumber
+    });
+    return user.save()
+}
+
+//  RETRIEVE models-----------------------
+//  by Filter
+const findUsers = async (filter) => {
+    const query = User.find(filter)
+    return query.exec()
+}
+//  by ID
+const findById = async (_id) => {
+    const query = User.findById(_id);
+    return query.exec();
+}
+
+//  UPDATE models------------------------
+//  DELETE models------------------------
+
+export {createUser, findUsers, findById}
